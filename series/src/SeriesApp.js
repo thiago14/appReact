6eae8685321c38cbe475a06120a233d05e1e4556
Thiago Mori <mori@quicktech.no>
2018-10-31 16:20:23 -0300

    Installed redux-thunk

--- series/src/SeriesApp.js
+++ series/src/SeriesApp.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import Router  from './Router'
 
 import rootReducers from './reducers'
 
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, applyMiddleware(reduxThunk))
 
 const SeriesApp = props => (
     <Provider store={store}>
